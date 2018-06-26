38d6d6cf430fcdfda9e666e0c80cfb35e7637123
Mustafa Kok <mustafakok@gmail.com>
2018-06-27 00:05:42 +0300

    Added, 03_UseEntityClassAndBinding project.

---
+++ PersonEditor/03_UseEntityClassAndBinding/FrmMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UseEntityClassAndBinding
+{
+    public partial class FrmMain : Form
+    {
+        BindingList<Person> m_PersonList;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            m_PersonList = new BindingList<Person>();
+            dgvPersons.AutoGenerateColumns = false;
+            dgvPersons.DataSource = m_PersonList;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Person _newPerson = new Person();
+            FrmEditPerson _frmEdit = new FrmEditPerson(_newPerson);
+
+            if(_frmEdit.ShowDialog() == DialogResult.OK)
+            { 
+                _newPerson.GUID = System.Guid.NewGuid().ToString();
+                m_PersonList.Add(_newPerson);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dgvPersons.SelectedRows.Count > 0)
+            {
+                var _selectedPerson = dgvPersons.SelectedRows[0].DataBoundItem as Person;
+                FrmEditPerson _frmEdit = new FrmEditPerson(_selectedPerson);
+                _frmEdit.ShowDialog();
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (dgvPersons.SelectedRows.Count > 0)
+            {
+                var _selectedPerson = dgvPersons.SelectedRows[0].DataBoundItem as Person;
+                var _confirmResult = MessageBox.Show($"Do you want to delete '{_selectedPerson.Name}'?", "Confirm delete", MessageBoxButtons.YesNo);
+                if(_confirmResult == DialogResult.Yes)
+                {
+                    m_PersonList.Remove(_selectedPerson);
+                }
+            }
+        }
+    }
+}
---
+++ PersonEditor/03_UseEntityClassAndBinding/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseEntityClassAndBinding
+{
+    public class Person : INotifyPropertyChanged
+    {
+        string m_Name;
+        string m_Surname;
+        DateTime m_DateOfBirth;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string GUID { get; set; }
+
+        public string Name
+        {
+            get { return m_Name; }
+            set
+            {
+                if(value != m_Name)
+                {
+                    m_Name = value;
+                    RaisePropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public string Surname
+        {
+            get { return m_Surname; }
+            set
+            {
+                if (value != m_Surname)
+                {
+                    m_Surname = value;
+                    RaisePropertyChanged(nameof(Surname));
+                }
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get { return m_DateOfBirth; }
+            set
+            {
+                if (value != m_DateOfBirth)
+                {
+                    m_DateOfBirth = value;
+                    RaisePropertyChanged(nameof(DateOfBirth));
+                }
+            }
+        }
+
+        public Person()
+        {
+            this.DateOfBirth = DateTime.Today;
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
